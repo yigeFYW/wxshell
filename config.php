a6fe1e7e6be828cb3255bc75328b27d38d0a3217
FYW <1015517471@qq.com>
2016-05-30 20:59:05 +0800

    del

--- config.php
+++ config.php
@@ -9,13 +9,13 @@ define('DB','weixin');//数据库 库名
 define('YM','hhsblog.cn');//网站域名
 */
 
-define('HOST','qdm203329110.my3w.com');//数据库主机地址
+define('HOST','localhost');//数据库主机地址
 define('PORT',3306);//主机端口号
-define('USER','qdm203329110');//数据库用户名
-define('PASSWORD','huanghedyx');//数据库密码
-define('DB','qdm203329110_db');//数据库 库名
+define('USER','root');//数据库用户名
+define('PASSWORD','');//数据库密码
+define('DB','weixin');//数据库 库名
 define('CHARSET','utf8');//字符集
 define('MYID','wx32cffe105b765454');
 define('MYSECRET','d4624c36b6795d1d99dcf0547af5443d');
-
+define("TOKEN", "weixin");//填入微信TOKEN
 ?>
